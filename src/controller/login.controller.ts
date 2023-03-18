a344de4ccb234d74c715be917ece534c416fb0c9
ssssshel <angelarteaga989@gmail.com>
2023-03-18 14:18:30 -0500

    migration to pnpm

--- src/controller/login.controller.ts
+++ src/controller/login.controller.ts
@@ -17,14 +17,14 @@ import generateTokens from "../services/generate_tokens.service";
 
 const userModel = UserDataModel
 
-export async function loginUser(req: any, res: Response<GenericServiceResponse | GenericServiceErrorResponse>){
-  const {user_email, user_password} = req.body
+export async function loginUser(req: any, res: Response<GenericServiceResponse | GenericServiceErrorResponse>) {
+  const { user_email, user_password } = req.body
 
   try {
-    const user = await userModel.findOne({where: {user_email: user_email}, include: {model: UserRoleModel, attributes: ["user_rol_name"]}})
+    const user = await userModel.findOne({ where: { user_email: user_email }, include: { model: UserRoleModel, attributes: ["user_rol_name"] } })
 
-    if(!user){
-      return res.status(401).json(status401Unauthorized("Invalid email"))  
+    if (!user) {
+      return res.status(401).json(status401Unauthorized("Invalid email"))
     }
     const data = user?.dataValues
 
@@ -32,19 +32,19 @@ export async function loginUser(req: any, res: Response<GenericServiceResponse |
       _id: data.id_user,
       rol: data.user_role.dataValues.user_rol_name,
       email: data.user_email,
-      
+
     }
     console.log(data)
     console.log(userData)
 
     const verifiedPassword = await bcrypt.compare(user_password, data.user_password)
-    if(!verifiedPassword){
+    if (!verifiedPassword) {
       return res.status(401).json(status401Unauthorized("Invalid password"))
     }
 
-    const {accessToken, refreshToken} = await generateTokens(userData)
-    
-    return res.status(200).json(status200Ok({accessToken: accessToken, refreshToken: refreshToken}, "", "User successfully logged"))
+    const { accessToken, refreshToken } = await generateTokens(userData)
+
+    return res.status(200).json(status200Ok({ accessToken: accessToken, refreshToken: refreshToken }, "", "User successfully logged"))
 
   } catch (error) {
     return res.status(500).json(status500InternalServerError(`${error}`))
